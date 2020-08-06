a44b61ad0313338407f8d955fe35f206f03a0c33
Walker Twyman <walk12288@gmail.com>
2020-08-06 14:58:43 -0500

    completed application style

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const imageInput = useRef(null);
   const [predictions, setPredictions] = useState([]);
   const [category, setCategory] = useState('');
+  const [accuracy, setAccuracy] = useState('');
+  const [canSubmit, setCanSubmit] = useState(false);
 
   const labels = [
     'Early Blight',
@@ -18,6 +20,14 @@ function App() {
   const handleFileChange = e =>{
     const file = imageInput.current.files[0];
 
+    if(!file)
+    {
+      setLocalImage('');
+      setAccuracy('');
+      setCanSubmit(false);
+      return;
+    }
+
     if (file && file.type && file.type.indexOf('image') === -1) {
       console.log('File is not an image.', file.type, file);
       return;
@@ -28,25 +38,73 @@ function App() {
       setLocalImage(event.target.result)
     });
     reader.readAsDataURL(file);
+
+    setCanSubmit(true);
   }
 
+  //Determine which of our categories are the largest
   const findHighestCategory = (results) =>{
+
     let highestIndex = 0;
+    let highestPercent = results[0] *100;
     
     for(let i = 1; i < results.length; i++){
       if(results[i] > results[highestIndex]){
         //update highest index if our results are highest
         highestIndex = i;
+        highestPercent = results[i] * 100;
       }
     }
 
-    return highestIndex;
+    return {labelIndex: highestIndex, percent: highestPercent};
+  }
+
+  const getBorderClass = () => {
+    
+    //Default
+    if(!accuracy){
+      return '';
+    }
+    
+    //Above 80
+    if(accuracy >= 80){
+      return 'green-border'
+    }
+    else if(accuracy > 50 && accuracy < 80)
+    {
+      return 'yellow-border'
+    }
+    else if(accuracy <= 50){
+      return 'red-border'
+    }
+  }
+
+  const getTextClass = () =>{
+    //Default
+    if(!accuracy){
+      return '';
+    }
+    
+    //Above 80
+    if(accuracy >= 80){
+      return 'green-text'
+    }
+    else if(accuracy > 50 && accuracy < 80)
+    {
+      return 'yellow-text'
+    }
+    else if(accuracy <= 50){
+      return 'red-text'
+    }
   }
 
   const handleResponseData = data =>{
-    const labelIndex = findHighestCategory(data.prediction);
-    setCategory(labels[labelIndex]);
+    // const labelIndex = findHighestCategory(data.prediction);
 
+    const {labelIndex, percent} = findHighestCategory(data.prediction);
+
+    setCategory(labels[labelIndex]);
+    setAccuracy(percent);
     setPredictions(data.prediction);
   }
 
@@ -74,6 +132,7 @@ function App() {
   const submitImage = (e) =>{
     e.preventDefault();
     console.log('submitting image to API');
+    setCanSubmit(false);
 
     var reader = new FileReader();
     reader.readAsDataURL(imageInput.current.files[0]);
@@ -83,14 +142,13 @@ function App() {
     };
     reader.onerror = function (error) {
       console.log('Error: ', error);
+      setCanSubmit(true);
     };
 
     
   }
 
   const generatePredictionData = () =>{
-    console.log('setting prediction list');
-
     if(predictions.length)
     {
 
@@ -132,18 +190,22 @@ function App() {
         <p>
           <strong>Please select a photo below to get started:</strong>
         </p>
-        <img src={localImage}/>
+        <img src={localImage} className={`plant-image ${getBorderClass()}`}/>
         <form>
           <input type="file" id="file-selector" accept=".jpg" ref={imageInput} onChange={handleFileChange} />
           
-          {imageInput && imageInput.current &&
+          {canSubmit &&
             <button onClick={submitImage}>Submit For Evaluation</button>
           }   
         </form>
 
         {/* Prediction */}
         {category &&
-          <h2>The disease is most likely {category}</h2>
+          <h2>The disease is most likely <span className={getTextClass()}>{category}</span></h2>
+        }
+
+        {category && accuracy <=50 &&
+          <p className={getTextClass()}>The accuracy of this prediction is very low. Try using a different photo.</p>
         }
 
         <div>
